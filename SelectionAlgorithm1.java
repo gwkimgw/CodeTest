51716238c16cbd014fa8c4add40157eb73877ca0
gwkimgw <marianaaquaria@gmail.com>
2022-10-03 12:12:37 +0900

    refactor: change quick selection code Modified given sudo code

--- SelectionAlgorithm1.java
+++ SelectionAlgorithm1.java
@@ -1,45 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Random;
 import java.util.StringTokenizer;
 
 public class SelectionAlgorithm1{
-    public static int select(int[] A, int p, int r, int q){
-        if(p == r){
-            return A[p];
-        }
-
-        int t = partition(A, p, r);
-        int k = t - p+1;
-
-        if(q < k){
-            return select(A, p, t-1, q);
-        } else if(q == k) {
-            return A[t];
-        } else {
-            return select(A, p, t+1, q);
-        }
-    }
-
-    public static int partition(int[] A, int p, int r){
-        int x = A[r];
-        int i = p-1;
-        for(int j=p;j<r-1;j++){
-            if(A[j]<=x){
-                swap(A, ++i, j);
-            }
-        }
-        if(i+1!=r){
-            swap(A, --i, i);
-        }
-        return i+1;
-    }
-
-    public static void swap(int[] A, int i, int j){
-        int temp = A[i];
-        A[i] = A[j];
-        A[j] = temp;
-    }
-
     public void solution() throws Exception{
         BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bReader.readLine());
@@ -53,7 +17,52 @@ public class SelectionAlgorithm1{
         for(int i=0;i<n;i++){
             arr[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
+
+        System.out.println(quickSelect(arr, 0, n-1, o));
     }
+
+	private static int quickSelect(int[] A, int start, int end, int k) {
+		if (start <= end) {
+			int pivot = partition(A, start, end);
+
+			if (pivot == k) {
+				return A[k];
+			}
+
+			else if (pivot > k) {
+				return quickSelect(A, start, pivot - 1, k);
+			}
+
+			else {
+				return quickSelect(A, pivot + 1, end, k);
+			}
+		}
+
+		return Integer.MIN_VALUE;
+	}
+
+	private static int partition(int[] A, int start, int end) {
+		int pivot = start + new Random().nextInt(end - start + 1);
+
+		swap(A, end, pivot);
+
+		for (int i = start; i < end; i++) {
+			if (A[i] > A[end]) {
+				swap(A, i, start);
+				start++;
+			}
+		}
+
+		swap(A, start, end);
+
+		return start;
+	}
+
+	private static void swap(int[] A, int i, int j) {
+		int tmp = A[i];
+		A[i] = A[j];
+		A[j] = tmp;
+	}
     
     public static void main(String[] args) throws Exception{
         new SelectionAlgorithm1().solution();
