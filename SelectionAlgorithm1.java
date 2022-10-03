40982bc0964882f4c9635762c8c61be9539f6ffc
gwkimgw <marianaaquaria@gmail.com>
2022-10-03 15:02:56 +0900

    feat: Add swapCnt

--- SelectionAlgorithm1.java
+++ SelectionAlgorithm1.java
@@ -3,28 +3,57 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class SelectionAlgorithm1{
-	public static int select(int[] A, int start, int end, int o){
-        int swapCnt = 0;
-        int pivot = partition(A, start, end);
-        int k = pivot-start+1;
+    public static int swapCnt = 0;
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bReader.readLine());
 
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int o = Integer.parseInt(stringTokenizer.nextToken());
+        int p = Integer.parseInt(stringTokenizer.nextToken());
+        
+        int[] arr = new int[n];
+
+        stringTokenizer = new StringTokenizer(bReader.readLine());
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+            
+        select(arr, 0, n-1, o, p);   
+    }
+
+	public static int select(int[] A, int start, int end, int o, int p){
+        int pivot = partition(A, start, end, p);
+        int k = pivot-start+1;
+        
         if(o<k){
-            return select(A, start, pivot-1, o);
+            return select(A, start, pivot-1, o, p);
         }else if(o==k){
             return A[pivot];
         }else{
-            return select(A, pivot+1, end, o-k);
+            return select(A, pivot+1, end, o-k, p);
         }
 	}
 
-	public static int partition(int[] A, int start, int end){
-		int x = A[end];
+	public static int partition(int[] A, int start, int end, int p){
+        int x = A[end];
         int point = start-1;
         
         for(int j=start;j<end-1;j++){
-            if(A[j]<=x) swap(A, ++point, j);
+            if(A[j]<=x){
+                swap(A, ++point, j);
+                if(++swapCnt == p){
+                    System.out.println(A[point] + " " + A[p]);
+                }
+            };
         }
-        if(point+1!=end) swap(A, point+1, end);
+        if(point+1!=end){
+            swap(A, point+1, end);
+            if(++swapCnt < p){
+                System.out.println(-1);
+            }
+        }
         
         return point+1;
     }
@@ -34,21 +63,4 @@ public class SelectionAlgorithm1{
 		A[i] = A[j];
 		A[j] = tmp;
 	}
-    
-    public static void main(String[] args) throws Exception{
-        BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bReader.readLine());
-
-        int n = Integer.parseInt(stringTokenizer.nextToken());
-        int o = Integer.parseInt(stringTokenizer.nextToken());
-        int p = Integer.parseInt(stringTokenizer.nextToken());
-        int[] arr = new int[n];
-
-        stringTokenizer = new StringTokenizer(bReader.readLine());
-        for(int i=0;i<n;i++){
-            arr[i] = Integer.parseInt(stringTokenizer.nextToken());
-        }
-
-        System.out.println(select(arr, 0, n-1, o));
-    }
 }
