e2693235f7caa3282fdd2d56895e3c4c1780020c
gwkimgw <marianaaquaria@gmail.com>
2022-10-21 14:24:22 +0900

    fix: Close scanner leakage Add try-catch for sc closing

--- Set1822.java
+++ Set1822.java
@@ -6,22 +6,22 @@ import java.util.*;
  
 public class Set1822 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        TreeSet<Integer> A = new TreeSet<Integer>();
-        
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int i, num;
-        for(i=0;i<a;i++)
-            A.add(sc.nextInt());
-        for(i=0;i<b;i++) {
-            num=sc.nextInt();
-            if(A.contains(num)) A.remove(num);
+        try (Scanner sc = new Scanner(System.in)) {
+            TreeSet<Integer> A = new TreeSet<Integer>();
+            
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int i, num;
+            for(i=0;i<a;i++) A.add(sc.nextInt());
+            for(i=0;i<b;i++) {
+                num=sc.nextInt();
+                if(A.contains(num)) A.remove(num);
+            }
+            
+            System.out.println(A.size());
+            for(int n : A) System.out.print(n+" ");
+            
+            sc.close();
         }
-        
-        System.out.println(A.size());
-        for(int n : A)
-            System.out.print(n+" ");
-        sc.close();
     }
 }
