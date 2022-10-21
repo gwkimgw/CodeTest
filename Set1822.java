78b004331c6f2dbb9be5db17da38d896cb5d8df1
gwkimgw <marianaaquaria@gmail.com>
2022-10-21 14:25:59 +0900

    refactor: Change try to throws Exception To match coding style

--- Set1822.java
+++ Set1822.java
@@ -5,23 +5,21 @@ import java.util.*;
 //백준 1822
  
 public class Set1822 {
-    public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            TreeSet<Integer> A = new TreeSet<Integer>();
-            
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int i, num;
-            for(i=0;i<a;i++) A.add(sc.nextInt());
-            for(i=0;i<b;i++) {
-                num=sc.nextInt();
-                if(A.contains(num)) A.remove(num);
-            }
-            
-            System.out.println(A.size());
-            for(int n : A) System.out.print(n+" ");
-            
-            sc.close();
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        TreeSet<Integer> A = new TreeSet<Integer>();
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int i, num;
+        for(i=0;i<a;i++) A.add(sc.nextInt());
+        for(i=0;i<b;i++) {
+            num=sc.nextInt();
+            if(A.contains(num)) A.remove(num);
         }
+        
+        System.out.println(A.size());
+        for(int n : A) System.out.print(n+" ");
+        sc.close();
     }
 }
