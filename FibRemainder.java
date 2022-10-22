7c98af41133ead5e793cc3eb559ac1d862badebd
gwkimgw <marianaaquaria@gmail.com>
2022-10-22 13:23:11 +0900

    refactor: STILL to big for int, reduce every step Save only remainder of every step

--- FibRemainder.java
+++ FibRemainder.java
@@ -3,9 +3,9 @@ public class FibRemainder {
         int[] rem = new int[n+1];
         rem[0] = 0; rem[1] = 1;
         
-        for(int j=2;j<n+1;j++) rem[j] = rem[j-1]+rem[j-2];
+        for(int j=2;j<n+1;j++) rem[j] = rem[j-1]%1234567+rem[j-2]%1234567;
         
-        return rem[n-1]%1234567+rem[n-2]%1234567;
+        return rem[n-1]+rem[n-2];
     }
     
     public int solution(int n) {
