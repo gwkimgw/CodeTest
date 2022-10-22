400abfce9e49ceb1c8a2a1886d47b0f8a39fc0b1
gwkimgw <marianaaquaria@gmail.com>
2022-10-22 13:24:58 +0900

    fix: Failed in some test case, divided once more Should return remainder of last returned fib(n)

--- FibRemainder.java
+++ FibRemainder.java
@@ -5,10 +5,10 @@ public class FibRemainder {
         
         for(int j=2;j<n+1;j++) rem[j] = rem[j-1]%1234567+rem[j-2]%1234567;
         
-        return rem[n-1]+rem[n-2];
+        return rem[n];
     }
     
     public int solution(int n) {
-        return fib(n);
+        return fib(n)%1234567;
     }
 }
