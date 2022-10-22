cb2871db9838aec6857903cf2691e2b76cb3003b
gwkimgw <marianaaquaria@gmail.com>
2022-10-22 13:21:03 +0900

    feat: Return remainder of nth fib div by 1234567

---
+++ FibRemainder.java
@@ -0,0 +1,14 @@
+public class FibRemainder {
+    public static int fib(int n){
+        int[] rem = new int[n+1];
+        rem[0] = 0; rem[1] = 1;
+        
+        for(int j=2;j<n+1;j++) rem[j] = rem[j-1]+rem[j-2];
+        
+        return rem[n-1]+rem[n-2];
+    }
+    
+    public int solution(int n) {
+        return fib(n)%1234567;
+    }
+}
