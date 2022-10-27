3fa4c14d8f983c8ddb0dc0f79e985eb9fe879e25
gwkimgw <marianaaquaria@gmail.com>
2022-10-27 19:24:31 +0900

    refactor: Seperated while loop Used more 'object-oriented programming'

--- NaturalSum.java
+++ NaturalSum.java
@@ -2,17 +2,20 @@ class NaturalSum {
     public int solution(int n) {
         int answer = 1;
         for(int i=1;i<=n/2;i++){
-            int count = i;
-            int sum = 0;
-            while(true){
-                sum+=count;
-                if(sum>=n){
-                    if(sum==n) answer++;
-                    break;
-                }
-                count++;
-            }
+            answer += sumNum(i, n);
         }
         return answer;
+    }
+    
+    public static int sumNum(int i, int n){
+        int sum = 0;
+        while(true){
+            sum+=i;
+            if(sum>=n){
+                if(sum==n) return 1;
+                return 0;
+            }
+            i++;
+        }
     }
 }
