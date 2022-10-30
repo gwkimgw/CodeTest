c77e1c244d6acc711ef16008777e533dce702687
gwkimgw <marianaaquaria@gmail.com>
2022-10-30 13:54:26 +0900

    feat: Sort people, min+max weight and check Pointer points at min weight. If boat reaches limit, point++ 'i' points at max weight. If i cannot be paired, i--

--- BoatLimit.java
+++ BoatLimit.java
@@ -3,21 +3,15 @@ import java.util.*;
 class BoatLimit {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        int l = people.length;
         Arrays.sort(people);
         
-        if(people[0]+people[1] > limit) return people.length;
+        if(people[0]+people[1] > limit) return l;
         
-        int check = 0;
-        for(int i: people){
-            if(check+i<limit){
-                check+=i;
-            }else if (check+i>limit){
-                answer+=2;
-                check=0;
-            }else{
-                answer++;
-                check=0;
-            }
+        int point = 0;
+        for(int i=l-1;point<=i;i--){
+            if(people[point] + people[i] <= limit) point++;
+            answer++;
         }
         
         return answer;
