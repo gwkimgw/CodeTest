ebee0957f79e0af5eae8d346417cf6c216025a1c
gwkimgw <marianaaquaria@gmail.com>
2022-11-11 14:49:39 +0900

    feat: Return chain order

--- MatrixChain.java
+++ MatrixChain.java
@@ -7,27 +7,58 @@ import java.util.stream.Stream;
 //백준 1822
 
 public class MatrixChain{
-	public static int matrixChain(int[] dims, int i, int j, int[][] lookup){
-		if (j <= i + 1) { return 0; }
+	private static void matrixChain(int[] dims) {
+        System.out.println(matrixChainOrder(dims));
+        System.out.println(getOrder());
+    }
 
-        int min = Integer.MAX_VALUE;
-        if (lookup[i][j] == 0){
-			for (int k = i + 1; k <= j - 1; k++){
-                int cost = matrixChain(dims, i, k, lookup);
-                cost += matrixChain(dims, k, j, lookup);
-                cost +=	dims[i] * dims[k] * dims[j];
-				if (cost < min) { min = cost; }
-			}
-			lookup[i][j] = min;
-		}
-        return lookup[i][j];
-	}
+    private static int[][]cost;
+    private static int[][]order;
+    
+    public static int matrixChainOrder(int[] dims) {
+        int n = dims.length - 1;
+        cost = new int[n][n];
+        order = new int[n][n];
+
+        for (int l = 1 ; l < n ; l++) {
+            for (int i = 0; i < n - l; i++) {
+                int j = i + l;
+                cost[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    int currentCost = cost[i][k] + cost[k+1][j] + dims[i]*dims[k+1]*dims[j+1];
+                    if (currentCost < cost[i][j]) {
+                        cost[i][j] = currentCost;
+                        order[i][j] = k;
+                    }
+                }
+            }
+        }
+        return cost[0][n-1];
+    }
+
+    private static String getOrder(){
+        return order(order, 0, order.length - 1);
+    }
+    
+    private static String order(int[][]s, int i, int j) {
+        if (i == j) {
+            return String.format("%c", i+65);
+        }
+        else {
+            StringBuilder sb = new StringBuilder();
+            sb.append("(");
+            sb.append(order(s, i, s[i][j]));
+            sb.append(order(s, s[i][j] + 1, j));
+            sb.append(")");
+            return sb.toString();
+        }
+    }
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int[] dims = Stream.of(br.readLine().split("x")).mapToInt(Integer::parseInt).toArray();
-        int[][] lookup = new int[dims.length][dims.length];
+        int[][] dp = new int[dims.length][dims.length];
 
-		System.out.print(matrixChain(dims, 0, dims.length - 1, lookup));
+		matrixChain(dims);
 	}
 }
