dc6ebaed1e87844d6087414fd8d87d280841ed43
gwkimgw <marianaaquaria@gmail.com>
2022-11-11 15:49:09 +0900

    refactor: Clensed up code Removed some duplicate functions

--- MatrixChain.java
+++ MatrixChain.java
@@ -7,48 +7,37 @@ import java.util.stream.Stream;
 //백준 1822
 
 public class MatrixChain{
-	private static void matrixChain(int[] dims) {
-        System.out.println(matrixChainOrder(dims));
-        System.out.println(getOrder());
-    }
-
-    private static int[][]cost;
-    private static int[][]order;
+    private static int[][] min;
+    private static int[][] order;
     
     public static int matrixChainOrder(int[] dims) {
-        int n = dims.length - 1;
-        cost = new int[n][n];
-        order = new int[n][n];
+        int n = dims.length-1;
+        min = new int[n][n]; order = new int[n][n];
 
-        for (int l = 1 ; l < n ; l++) {
-            for (int i = 0; i < n - l; i++) {
-                int j = i + l;
-                cost[i][j] = Integer.MAX_VALUE;
-                for (int k = i; k < j; k++) {
-                    int currentCost = cost[i][k] + cost[k+1][j] + dims[i]*dims[k+1]*dims[j+1];
-                    if (currentCost < cost[i][j]) {
-                        cost[i][j] = currentCost;
+        for (int l=1;l<n;l++) {
+            for (int i=0;i<n-l;i++){
+                int j = i+l;
+                min[i][j] = Integer.MAX_VALUE;
+                for(int k=i;k<j;k++){
+                    int cost = min[i][k] + min[k+1][j] + dims[i]*dims[k+1]*dims[j+1];
+                    if(cost < min[i][j]){
+                        min[i][j] = cost;
                         order[i][j] = k;
                     }
                 }
             }
         }
-        return cost[0][n-1];
+        return min[0][n-1];
     }
 
-    private static String getOrder(){
-        return order(order, 0, order.length - 1);
-    }
-    
-    private static String order(int[][]s, int i, int j) {
-        if (i == j) {
-            return String.format("%c", i+65);
-        }
-        else {
+    private static String getOrder(int[][]s, int i, int j) {
+        if (i == j){
+            return "A" + Integer.toString(i+1);
+        }else{
             StringBuilder sb = new StringBuilder();
             sb.append("(");
-            sb.append(order(s, i, s[i][j]));
-            sb.append(order(s, s[i][j] + 1, j));
+            sb.append(getOrder(s, i, s[i][j]));
+            sb.append(getOrder(s, s[i][j] + 1, j));
             sb.append(")");
             return sb.toString();
         }
@@ -56,9 +45,10 @@ public class MatrixChain{
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] dims = Stream.of(br.readLine().split("x")).mapToInt(Integer::parseInt).toArray();
-        int[][] dp = new int[dims.length][dims.length];
-
-		matrixChain(dims);
+		int[] dims = Stream.of(br.readLine().split("x"))
+			.mapToInt(Integer::parseInt)
+			.toArray();
+        System.out.println(matrixChainOrder(dims)
+			+ "\n" + getOrder(order, 0, order.length-1));
 	}
 }
