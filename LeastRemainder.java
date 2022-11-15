324b74d6f5367484e633bf0c7b00bdf6fc177224
gwkimgw <marianaaquaria@gmail.com>
2022-11-15 12:53:42 +0900

    feat: Return smallest num to return 1 as remainder

---
+++ LeastRemainder.java
@@ -0,0 +1,11 @@
+public class LeastRemainder{
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=n-1;i>0;i--){
+            if(n%i == 1){
+                answer = i;
+            }
+        }
+        return answer;
+    }
+}
