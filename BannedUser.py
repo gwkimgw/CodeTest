ff002eb6cc5a9bbd5c7568314ff2280c4da48acf
gwkimgw <marianaaquaria@gmail.com>
2022-12-06 13:48:14 +0900

    fix: Replace 'user id' for index

--- BannedUser.py
+++ BannedUser.py
@@ -2,8 +2,9 @@ import re
 from itertools import product
 
 def solution(user_id, banned_id):
-    m = ([j for j in user_id if re.match(f"^{i.replace('*', '.')}$", j)]
+    m = ([j for j, jj in enumerate(user_id) if re.match(f"^{i.replace('*', '.')}$", jj)]
         for i in banned_id)
+
     s = (set(k) for k in product(*m))
     s = set(tuple(l) for l in s if len(l) == len(banned_id))
     return len(s)
